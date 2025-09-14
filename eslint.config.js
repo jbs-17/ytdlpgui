7911be4f2ba815fcd894beff2c5961b37ac6f056
jatibanguns <jatibangun17@gmail.com>
2025-09-14 20:57:56 +0700

    initialisasi project; setup project;

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,14 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs}"],
+    ignores: ["node_modules/**", "tmp/**"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    extends: [js.configs.recommended]
+  }
 ]);
